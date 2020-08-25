fd17f5784c0163afeea637ee71d271a965a96f85
Flavia <flaviamatta90@gmail.com>
2020-08-25 15:17:43 +0200

    total firstname

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert("ciao");
+// 1 Chiedere all’utente il cognome
+var cognome = prompt('Scrivi il tuo cognome');
+
+// 2 Inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+var altriCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+var totale = altriCognomi.push(cognome);
+console.log(altriCognomi);
+
+// 3 stampa la lista ordinata alfabeticamente
+
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
