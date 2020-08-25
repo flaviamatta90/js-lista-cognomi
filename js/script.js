43bbfa316e6e35e5c3986ddaf2196bb0b45c093e
Flavia <flaviamatta90@gmail.com>
2020-08-25 17:43:45 +0200

    point 4/5

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-// 1 Chiedere all’utente il cognome
-var cognome = prompt('Scrivi il tuo cognome');
+// // 1 Chiedere all’utente il cognome
+var cognomeUtente = prompt('Scrivi il tuo cognome').toUpperCase();
 
-// 2 Inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
-var altriCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
-var totale = altriCognomi.push(cognome);
-altriCognomi.sort(); 
-console.log(altriCognomi);
+while (cognomeUtente.length <= 0) {
+  cognomeUtente = prompt("Scrivi il tuo Cognome").toUpperCase();
+}
 
-// 3 stampa la lista ordinata alfabeticamente
+// // 2 Inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
-// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+// 3 Aggiungo il cognome utente nell'array
+cognomi.push(cognomeUtente);
+
+// 4 stampa la lista ordinata alfabeticamente
+cognomi.sort();
+for (var i = 0; i < cognomi.length; i++) {
+  document.getElementById("lista-cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
+}
+
+// 5 Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+var posizione = cognomi.indexOf(cognomeUtente) + 1;
+console.log('Sei il numero ' + posizione)
